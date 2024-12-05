03c4b2da3c41268df2aa7f80e203f6a204972360
Pheera6509650617 <pheera.phu@dome.tu.ac.th>
2024-12-05 16:31:32 +0700

    FIX-TEST

--- client/jest.config.js
+++ client/jest.config.js
@@ -38,10 +38,10 @@ module.exports = {
   testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/.next/'],
   testEnvironment: 'jsdom',
   transform: {
-    // Use babel-jest to transpile tests with the next/babel preset
-    // https://jestjs.io/docs/configuration#transform-objectstring-pathtotransformer--pathtotransformer-object
-    '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }],
-    '^.+\\.jsx?$': 'babel-jest',
+  // ใช้ babel-jest พร้อม next/babel preset
+  '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'], configFile: './jest.babel.config.js' }],
+  // กรณีเฉพาะไฟล์ .jsx (หากต้องการ)
+  '^.+\\.jsx?$': 'babel-jest',
   },
   transformIgnorePatterns: [
     '/node_modules/',
